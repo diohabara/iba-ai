06feca40f4695691e76137b6a4105b0b358a4798
diohabara <diohabara@gmail.com>
2021-11-24 21:45:58 +0900

    init: initialize problem4

---
+++ problem4/README.md
@@ -0,0 +1,22 @@
+# 課題番号 04
+
+## `BFS` 実行方法
+
+```bash
+make bfs
+./bfs <integer>
+```
+
+## `DFS` 実行方法
+
+```bash
+make dfs
+./dfs <integer>
+```
+
+## `A*` 実行方法
+
+```bash
+make astar
+./astar <integer>
+```
---
+++ problem4/astar.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "bucket.h"
+
+bool is_integer(std::string str) {
+  for (size_t i = 0; i < str.size(); i++)
+    if (isdigit(str[i]) == false) return false;
+  return true;
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << "the number of buckets" << std::endl;
+    return 1;
+  }
+  if (!is_integer(argv[1])) {
+    std::cerr << argv[1] << " is not an integer" << std::endl;
+    std::cerr << "Usage: " << argv[0] << "the number of buckets" << std::endl;
+    return 1;
+  }
+  int n = std::stoi(argv[1]);
+  std::cout << "This is the answer of " << n << std::endl;
+  auto bucket_list = BucketList(n);
+  auto number_of_nodes = bucket_list.a_star_solve();
+  std::cout << "The number of nodes is " << number_of_nodes << std::endl;
+  return 0;
+}
